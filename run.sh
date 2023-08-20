46dce90a320643b3c64fd7d58d3c92f3104613e9
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-08-20 20:55:45 +0530

    add some other changed to the arguments

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-python -u main.py --method=coop \ 
-    --dataset-dir=dataset \
-    --device=cuda\
-    --clip-model=RN50x16\
-    --batch-size=8\
-    --epochs=2\
-    --lr=5e-5\
-    --n-ctx=16
+python -u main.py
